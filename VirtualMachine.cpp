c3cbb63085bab83137abddcfb8ffd2c1d227f24d
edmondmw <edmondmwang@gmail.com>
2015-04-28 19:42:05 -0700

    Sleep.so for a single thread

--- VirtualMachine.cpp
+++ VirtualMachine.cpp
@@ -1,6 +1,7 @@
 #include "VirtualMachine.h"
 #include "Machine.h"
 #include <unistd.h>
+#include <iostream>
 /*#define VM_THREAD_STATE_DEAD 	((TVMTreadState) 0x00)
 #define VM_THREAD_STATE_RUNNING ((TVMTreadState) 0x01)
 #define VM_THREAD_STATE_READY 	((TVMTreadState) 0x02)
@@ -17,7 +18,15 @@
 */
 
 extern "C"{
+    volatile TVMTick globalTick;
+
     TVMMainEntry VMLoadModule(const char *module);
+
+    void AlarmRequestCallback(void *var)
+    {
+        globalTick--;
+        //std::cout<<"global tick: "<<globalTick<<std::endl;
+    }
 
     TVMStatus VMStart(int tickms, int machinetickms, int argc, char *argv[])
 	{
@@ -29,6 +38,9 @@ extern "C"{
 
         //load the module
 		VMMain = VMLoadModule(argv[0]);	
+
+        MachineInitialize(machinetickms);
+        MachineRequestAlarm(tickms*1000,(TMachineAlarmCallback)AlarmRequestCallback,NULL);
 
         //if valid address
         if(VMMain != NULL)
@@ -45,7 +57,20 @@ extern "C"{
 
     TVMStatus VMThreadSleep(TVMTick tick)
     {
+        if(tick == VM_TIMEOUT_INFINITE)
+        {
+            return VM_STATUS_ERROR_INVALID_PARAMETER;
+        }
 
+        //add if VM_TIMEOUT_ERROR
+
+        globalTick = tick;
+
+        //thread is sleeping until globalTick is set to zero
+        while(0 != globalTick)
+        {    
+        }  
+        return VM_STATUS_SUCCESS;
     }
 
 
